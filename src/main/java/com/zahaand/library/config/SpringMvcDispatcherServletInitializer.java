a89e339bd37bbcaa5e397f3f32d6c9c1b7e6639b
Andrey Zakharenkov <zahaand@gmail.com>
2022-08-18 15:21:59 +0300

    add spring configuration

---
+++ src/main/java/com/zahaand/library/config/SpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,17 @@
+package com.zahaand.library.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
