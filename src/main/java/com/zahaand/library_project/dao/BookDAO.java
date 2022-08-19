44dc9bc246bb05f5ab855b2f67d15629018ac7ff
Andrey Zakharenkov <zahaand@gmail.com>
2022-08-19 12:40:19 +0300

    add DAOs

---
+++ src/main/java/com/zahaand/library_project/dao/BookDAO.java
@@ -0,0 +1,53 @@
+package com.zahaand.library_project.dao;
+
+import com.zahaand.library_project.models.Book;
+import com.zahaand.library_project.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class BookDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Book> getBooksList() {
+        return jdbcTemplate.query("SELECT * FROM books", new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    public Book getBook(int id) {
+        return jdbcTemplate.query("SELECT * FROM books WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Book.class)).stream().findAny().orElse(null);
+    }
+
+    public void saveBook(Book book) {
+        jdbcTemplate.update("INSERT INTO books(title, author, publishing_year) VALUES (?, ?, ?)", book.getTitle(), book.getAuthor(), book.getPublishingYear());
+    }
+
+    public void updateBook(int id, Book updatedBook) {
+        jdbcTemplate.update("UPDATE books SET title=?, author=?, publishing_year=? WHERE id=?", updatedBook.getTitle(), updatedBook.getAuthor(), updatedBook.getPublishingYear(), id);
+    }
+
+    public void deleteBook(int id) {
+        jdbcTemplate.update("DELETE FROM books WHERE id=?", id);
+    }
+
+    public Optional<Person> getBookOwner(int id) {
+        return jdbcTemplate.query("SELECT people.* FROM books JOIN people ON books.person_id = people.id WHERE book.id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
+    }
+
+    public void assignBook(int id, Person person) {
+        jdbcTemplate.update("UPDATE books SET person_id=? WHERE id=?", person.getId(), id);
+    }
+
+    public void releaseBook(int id) {
+        jdbcTemplate.update("UPDATE books SET person_id=null WHERE id=?", id);
+    }
+}
